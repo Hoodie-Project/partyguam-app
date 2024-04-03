62d93e12d455e49c76b8283d49f077b37779c4a7
ha-ccoon <codeminhasohn@gmail.com>
2024-04-03 23:32:44 +0900

    chore: add menustyle to dropDownMenu

--- lib/app/screens/sign_up_details/widgets/drop_down_menu.dart
+++ lib/app/screens/sign_up_details/widgets/drop_down_menu.dart
@@ -17,49 +17,90 @@ class _AddressDropDownMenuState extends State<AddressDropDownMenu> {
     return Padding(
       padding: EdgeInsets.only(left: 20.0, right: 20.0),
       child: DropdownMenu(
-        width: 335,
+        initialSelection: ProvinceMockData.seoul,
+        width: 335.0,
         hintText: hintText,
-        inputDecorationTheme: InputDecorationTheme(
-            contentPadding: const EdgeInsets.only(left: 20.0),
-            hintStyle: TextStyle(
-              color: AppColors.greyColors.shade400,
-              fontSize: 16,
-              fontWeight: FontWeight.normal,
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(
-                color: AppColors.greyColors.shade200,
-                width: 1.0,
-              ),
-              borderRadius: const BorderRadius.all(
-                Radius.circular(16.0),
-              ),
-            ),
-            focusedBorder: const OutlineInputBorder(
-              borderSide: BorderSide(
-                color: AppColors.primaryLightColors,
-                width: 1.0,
-              ),
-              borderRadius: BorderRadius.all(
-                Radius.circular(16.0),
-              ),
-            )),
         textStyle: TextStyle(
-          color: AppColors.greyColors.shade100,
-          fontSize: 16,
           fontWeight: FontWeight.normal,
+          fontSize: 16.0,
         ),
-        dropdownMenuEntries: ColorLabel.values
-            .map<DropdownMenuEntry<ColorLabel>>((ColorLabel color) {
-          return DropdownMenuEntry<ColorLabel>(
-            value: color,
-            label: color.label,
-            enabled: color.label != 'Grey',
-            style: MenuItemButton.styleFrom(
-              foregroundColor: color.color,
+        menuHeight: 156.0,
+        menuStyle: MenuStyle(
+          // alignment: Alignment.centerLeft,
+          backgroundColor: MaterialStateProperty.resolveWith(
+            (states) => Colors.white,
+          ),
+          minimumSize: MaterialStateProperty.resolveWith(
+            (states) => Size(335.0, 156.0),
+          ),
+          shape: MaterialStateProperty.resolveWith(
+            (states) => RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16.0),
+              side: BorderSide(color: AppColors.primaryLightColors),
             ),
-          );
-        }).toList(),
+          ),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          contentPadding: const EdgeInsets.only(left: 20.0),
+          hintStyle: TextStyle(
+            color: AppColors.greyColors.shade400,
+            fontSize: 16.0,
+            fontWeight: FontWeight.normal,
+          ),
+          // border: OutlineInputBorder(
+          //   borderSide: BorderSide(
+          //     color: Colors.blue,
+          //     width: 1.0,
+          //   ),
+          //   borderRadius: const BorderRadius.all(
+          //     Radius.circular(16.0),
+          //   ),
+          // ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.blue,
+              width: 1.0,
+            ),
+            borderRadius: const BorderRadius.all(
+              Radius.circular(16.0),
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.red,
+              width: 1.0,
+            ),
+            borderRadius: BorderRadius.all(
+              Radius.circular(16.0),
+            ),
+          ),
+        ),
+        // menuStyle: MenuStyle(),
+        selectedTrailingIcon: const Icon(
+          Icons.keyboard_arrow_up_rounded,
+          color: AppColors.primaryLightColors,
+        ),
+        trailingIcon: Icon(
+          Icons.keyboard_arrow_down_rounded,
+          color: AppColors.greyColors.shade300,
+        ),
+        dropdownMenuEntries:
+            ProvinceMockData.values.map<DropdownMenuEntry<ProvinceMockData>>(
+          (ProvinceMockData province) {
+            return DropdownMenuEntry(label: province.label, value: province);
+          },
+        ).toList(),
+        // dropdownMenuEntries: ColorLabel.values
+        //     .map<DropdownMenuEntry<ColorLabel>>((ColorLabel color) {
+        //   return DropdownMenuEntry<ColorLabel>(
+        //     value: color,
+        //     label: color.label,
+        //     enabled: color.label != 'Grey',
+        //     style: MenuItemButton.styleFrom(
+        //       foregroundColor: color.color,
+        //     ),
+        //   );
+        // }).toList(),
       ),
     );
   }
