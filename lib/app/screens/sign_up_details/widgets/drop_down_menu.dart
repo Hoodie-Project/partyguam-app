36fb1bbebcd474c53a775aeb7dfb2ec78d07fe0c
ha-ccoon <codeminhasohn@gmail.com>
2024-04-05 18:48:53 +0900

    chore: create a dropDownMenu - complete

--- lib/app/screens/sign_up_details/widgets/drop_down_menu.dart
+++ lib/app/screens/sign_up_details/widgets/drop_down_menu.dart
@@ -10,87 +10,92 @@ class AddressDropDownMenu extends StatefulWidget {
 }
 
 class _AddressDropDownMenuState extends State<AddressDropDownMenu> {
-  static const String hintText = '시·도를 선택해 주세요';
+  final TextEditingController textController = TextEditingController();
+  static const String hintText = '시·도';
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.only(left: 20.0, right: 20.0),
-      child: DropdownMenu(
-        initialSelection: ProvinceMockData.seoul,
-        width: 335.0,
-        hintText: hintText,
-        textStyle: TextStyle(
-          fontWeight: FontWeight.normal,
-          fontSize: 16.0,
-        ),
-        menuHeight: 156.0,
-        menuStyle: MenuStyle(
-          // alignment: Alignment.centerLeft,
-          // backgroundColor: MaterialStateProperty.resolveWith(
-          //   (states) => AppColors.greyColors.shade50,
-          // ),
-          minimumSize: MaterialStateProperty.resolveWith(
-            (states) => Size(335.0, 156.0),
-          ),
-          shape: MaterialStateProperty.resolveWith(
-            (states) => RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16.0),
-              side: BorderSide(color: AppColors.primaryLightColors),
+      padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+      child: Material(
+        elevation: 1.0,
+        borderRadius: BorderRadius.circular(16.0),
+        color: AppColors.greyColors.shade50,
+        child: DropdownMenu(
+          controller: textController,
+          dropdownMenuEntries:
+              ProvinceMockData.values.map<DropdownMenuEntry<ProvinceMockData>>(
+            (ProvinceMockData province) {
+              return DropdownMenuEntry(label: province.label, value: province);
+            },
+          ).toList(),
+          hintText: hintText,
+          inputDecorationTheme: InputDecorationTheme(
+            contentPadding: const EdgeInsets.only(
+              left: 20.0,
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: AppColors.greyColors.shade300,
+                width: 1.0,
+              ),
+              borderRadius: const BorderRadius.all(
+                Radius.circular(16.0),
+              ),
+            ),
+            focusedBorder: const OutlineInputBorder(
+              borderSide: BorderSide(
+                color: AppColors.primaryLightColors,
+                width: 1.0,
+              ),
+              borderRadius: BorderRadius.all(
+                Radius.circular(16.0),
+              ),
+            ),
+            hintStyle: TextStyle(
+              color: AppColors.greyColors.shade400,
+              fontSize: 16.0,
+              fontWeight: FontWeight.normal,
             ),
           ),
-        ),
-        inputDecorationTheme: InputDecorationTheme(
-          contentPadding:
-              const EdgeInsets.only(left: 20.0, top: 15.0, bottom: 15.0),
-          hintStyle: TextStyle(
-            color: AppColors.greyColors.shade400,
-            fontSize: 16.0,
+          menuHeight: 156.0,
+          menuStyle: MenuStyle(
+            /// TODO: 메뉴 backgroundColor 하얀색으로 지정
+            backgroundColor: MaterialStateProperty.resolveWith(
+              (states) {
+                return AppColors.greyColors.shade50;
+              },
+            ),
+            minimumSize: MaterialStateProperty.resolveWith(
+              (states) {
+                return const Size(161.0, 52.0);
+              },
+            ),
+            shape: MaterialStateProperty.resolveWith(
+              (states) {
+                return RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16.0),
+                  side: const BorderSide(color: AppColors.primaryLightColors),
+                );
+              },
+            ),
+          ),
+          // menuStyle: MenuStyle(),
+          selectedTrailingIcon: const Icon(
+            Icons.keyboard_arrow_up_rounded,
+            color: AppColors.primaryLightColors,
+          ),
+          textStyle: const TextStyle(
             fontWeight: FontWeight.normal,
+            fontSize: 16.0,
           ),
-          // border: OutlineInputBorder(
-          //   borderSide: BorderSide(
-          //     color: Colors.blue,
-          //     width: 1.0,
-          //   ),
-          //   borderRadius: const BorderRadius.all(
-          //     Radius.circular(16.0),
-          //   ),
-          // ),
-          enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(
-              color: AppColors.greyColors.shade300,
-              width: 1.0,
-            ),
-            borderRadius: const BorderRadius.all(
-              Radius.circular(16.0),
-            ),
+          trailingIcon: Icon(
+            Icons.keyboard_arrow_down_rounded,
+            color: AppColors.greyColors.shade300,
           ),
-          focusedBorder: const OutlineInputBorder(
-            borderSide: BorderSide(
-              color: AppColors.primaryLightColors,
-              width: 1.0,
-            ),
-            borderRadius: BorderRadius.all(
-              Radius.circular(16.0),
-            ),
-          ),
+          requestFocusOnTap: true,
+          width: 161.0,
         ),
-        // menuStyle: MenuStyle(),
-        selectedTrailingIcon: const Icon(
-          Icons.keyboard_arrow_up_rounded,
-          color: AppColors.primaryLightColors,
-        ),
-        trailingIcon: Icon(
-          Icons.keyboard_arrow_down_rounded,
-          color: AppColors.greyColors.shade300,
-        ),
-        dropdownMenuEntries:
-            ProvinceMockData.values.map<DropdownMenuEntry<ProvinceMockData>>(
-          (ProvinceMockData province) {
-            return DropdownMenuEntry(label: province.label, value: province);
-          },
-        ).toList(),
       ),
     );
   }
