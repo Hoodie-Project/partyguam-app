5b24eaf741dfc3b9f849df8aeaf0691b86bc4eda
ha-ccoon <codeminhasohn@gmail.com>
2024-04-09 00:24:31 +0900

    chore: decorate a SignUpDetailsStepper
    - add color to the step
    - the color of disabled steps can't be changed
    https://github.com/flutter/flutter/issues/102558

--- lib/app/screens/sign_up_details/widgets/steppers.dart
+++ lib/app/screens/sign_up_details/widgets/steppers.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:partyguam/app/theme/colors.dart';
 
 class SignUpDetailsStepper extends StatefulWidget {
   const SignUpDetailsStepper({super.key});
@@ -12,26 +13,86 @@ class _SignUpDetailsStepperState extends State<SignUpDetailsStepper> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 200,
-      child: Stepper(
-        elevation: 0,
-        currentStep: currentStep,
-        type: StepperType.horizontal,
-        steps: [
-          Step(
-            title: Text('관심지역'),
-            content: Container(),
+    List<Step> getSteps = [
+      Step(
+        state: currentStep > 0 ? StepState.complete : StepState.indexed,
+        isActive: currentStep >= 0,
+        title: const Text(
+          '관심지역',
+          style: TextStyle(
+            fontSize: 12.0,
+            fontWeight: FontWeight.w700,
           ),
-          Step(
-            title: Text('경력&포지션'),
-            content: Container(),
+        ),
+        content: Container(),
+      ),
+      Step(
+        state: currentStep > 1 ? StepState.complete : StepState.indexed,
+        isActive: currentStep >= 1,
+        title: const Text(
+          '경력&포지션',
+          style: TextStyle(
+            fontSize: 12.0,
+            fontWeight: FontWeight.w700,
           ),
-          Step(
-            title: Text('성향선택(1/4)'),
-            content: Container(),
+        ),
+        content: Container(),
+      ),
+      Step(
+        state: currentStep > 2 ? StepState.complete : StepState.indexed,
+        isActive: currentStep >= 2,
+        title: const Text(
+          '성향선택(1/4)',
+          style: TextStyle(
+            fontSize: 12.0,
+            fontWeight: FontWeight.w700,
           ),
-        ],
+        ),
+        content: Container(),
+      ),
+    ];
+
+    return Theme(
+      data: Theme.of(context).copyWith(
+        canvasColor: AppColors.greyColors.shade50,
+        colorScheme: Theme.of(context).colorScheme.copyWith(
+              primary: AppColors.primaryLightColors,
+            ),
+      ),
+      child: Container(
+        height: 200,
+        child: Stepper(
+          elevation: 0,
+          connectorThickness: 2.0,
+          // controlsBuilder: (BuildContext context, ControlsDetails details) {
+          //   return Container();
+          // },
+          currentStep: currentStep,
+          onStepContinue: () {
+            final isLastStep = currentStep == getSteps.length - 1;
+            setState(
+              () {
+                if (isLastStep) {
+                  /// 데이터 전송
+                  print('completed');
+                } else {
+                  currentStep += 1;
+                }
+              },
+            );
+          },
+          onStepCancel: () {
+            currentStep == 0
+                ? null
+                : setState(
+                    () {
+                      currentStep -= 1;
+                    },
+                  );
+          },
+          type: StepperType.horizontal,
+          steps: getSteps,
+        ),
       ),
     );
   }
