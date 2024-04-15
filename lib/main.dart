dae2a8b239b2e79b4e11355281e46791fdf47963
ha-ccoon <codeminhasohn@gmail.com>
2024-04-16 00:13:00 +0900

    chore: implement kakaoSDK
    - add kakaoSDK to activity

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
 import 'package:partyguam/app/route.dart';
 import 'package:partyguam/app/theme/theme.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
+  // runApp() 호출 전 Flutter SDK 초기화
+  KakaoSdk.init(
+    nativeAppKey: '${dotenv.env['APP_KEY']}',
+    javaScriptAppKey: '${dotenv.env['APP_KEY']}',
+  );
+
   runApp(const MyApp());
 }
 
