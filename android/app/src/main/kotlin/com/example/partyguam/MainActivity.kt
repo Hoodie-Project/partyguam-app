68bb77abc1e8f1b450401efd8b657a54eb19546f
ha-ccoon <codeminhasohn@gmail.com>
2024-04-24 12:46:35 +0900

    chore: change package name in android
    - change to com.hoodie.partyguam
    - set it for kakaoSDK

--- android/app/src/main/kotlin/com/example/partyguam/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.partyguam
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/hoodie/partyguam/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.hoodie.partyguam
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
