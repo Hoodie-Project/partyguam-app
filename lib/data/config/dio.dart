ed59e28677d37c9320501a5a0b82052e34470fdd
ha-ccoon <codeminhasohn@gmail.com>
2024-04-25 20:32:41 +0900

    chore: create sendUserInfo functionality
    - add ApiPath

--- lib/data/config/dio.dart
+++ lib/data/config/dio.dart
@@ -1,10 +1,10 @@
 import 'package:dio/dio.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:partyguam/data/config/path.dart';
+
+final dio = Dio(options);
 
 final options = BaseOptions(
-  baseUrl: dotenv.env['HOST_URL'] as String,
+  baseUrl: ApiConfigPath.hostUri,
   connectTimeout: const Duration(seconds: 5),
   receiveTimeout: const Duration(seconds: 3),
 );
-
-final dio = Dio(options);
