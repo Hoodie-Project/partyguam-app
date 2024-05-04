6e3ad8b2b4bae2fcc26174f54e4c358f40bd0917
ha-ccoon <codeminhasohn@gmail.com>
2024-05-04 15:48:28 +0900

    chore: modify signUp text button
    - move the widgets from components to login_0000

--- lib/app/pages/sign_up/views/login_0000.dart
+++ lib/app/pages/sign_up/views/login_0000.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
+import '../../../../domain/usecases/user_usecase.dart';
 import '../../../theme/colors.dart';
 import '../../../theme/icons.dart';
 import '../widgets/app_bar.dart';
-import '../widgets/components.dart';
 import '../widgets/text.dart';
 
 class Login0000 extends StatefulWidget {
@@ -30,31 +31,114 @@ class _Login0000State extends State<Login0000> {
                 mainTitle: '파티괌과 함께\n파티에 참여할 준비가 되셨나요?',
                 subTitle: '소셜 로그인으로 편하게 이용해보세요.',
               ),
-              SocialLoginButton(
-                text: '카카오톡 로그인',
-                icon: kakaoIcon,
-                backgroundColor: const Color(0XFFFEE500),
+              _buildSocialLoginButton(
+                '카카오톡 로그인',
+                const Color(0XFFFEE500),
+                kakaoIcon,
               ),
               const SizedBox(
                 height: 8.0,
               ),
-              SocialLoginButton(
-                text: '구글 로그인',
-                icon: googleIcon,
-                backgroundColor: AppColors.greyColors.shade50,
+              _buildSocialLoginButton(
+                '구글 로그인',
+                AppColors.greyColors.shade50,
+                googleIcon,
               ),
-              const Padding(
-                padding: EdgeInsets.only(top: 32.0),
-                child: TermText(),
+              Padding(
+                padding: const EdgeInsets.only(top: 32.0),
+                child: _buildTermText(),
               ),
-              const Padding(
-                padding: EdgeInsets.only(top: 24.0),
-                child: SignUpTextButton(),
+              Padding(
+                padding: const EdgeInsets.only(top: 24.0),
+                child: _buildInquiryText(),
               ),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildSocialLoginButton(
+      String text, Color backgroundColor, Widget icon) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton.icon(
+        onPressed: () async {
+          if (text == '카카오톡 로그인') {
+            await signInWithKakao(context);
+          } else if (text == '구글 로그인') {
+            context.push('/sign_up/0111');
+          }
+        },
+        style: ElevatedButton.styleFrom(
+          alignment: Alignment.centerLeft,
+          backgroundColor: backgroundColor,
+          elevation: 1,
+          foregroundColor: AppColors.greyColors.shade700,
+          padding: const EdgeInsets.only(
+            left: 20.0,
+          ),
+          minimumSize: const Size(
+            335.0,
+            52.0,
+          ),
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(16.0),
+            ),
+          ),
+          textStyle: const TextStyle(
+            fontSize: 14.0,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        icon: icon,
+        label: Text(text),
+      ),
+    );
+  }
+
+  Widget _buildTermText() {
+    return RichText(
+      text: TextSpan(
+        text: '소셜 로그인 가입 시 ',
+        style: TextStyle(
+          color: AppColors.greyColors.shade700,
+          fontSize: 12.0,
+          fontWeight: FontWeight.normal,
+        ),
+        children: const [
+          TextSpan(
+            text: '이용약관 개인정보처리방침 전자금융거래약관 \n결제/환불약관',
+            style: TextStyle(
+              fontSize: 12.0,
+              fontWeight: FontWeight.normal,
+              decoration: TextDecoration.underline,
+            ),
+          ),
+          TextSpan(text: '에 동의한 것으로 간주합니다.')
+        ],
+      ),
+    );
+  }
+
+  Widget _buildInquiryText() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        TextButton(
+          onPressed: () {},
+          child: Text(
+            '문의하기',
+            style: TextStyle(
+              color: AppColors.greyColors.shade500,
+              fontSize: 12.0,
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
