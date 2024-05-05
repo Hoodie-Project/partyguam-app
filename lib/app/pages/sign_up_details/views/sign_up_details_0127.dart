4d771183b7460f226bb116ae137bf8ad06a1d506
ha-ccoon <codeminhasohn@gmail.com>
2024-05-06 00:50:39 +0900

    feat: create a detailed profile completion page

--- lib/app/pages/sign_up_details/views/sign_up_details_0127.dart
+++ lib/app/pages/sign_up_details/views/sign_up_details_0127.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 import '../../../widgets/app_bar.dart';
-import '../widgets/steppers.dart';
+import '../../../widgets/buttons.dart';
+import '../../sign_up/views/styles.dart';
 
 class SignUpDetail0127 extends StatefulWidget {
   const SignUpDetail0127({super.key});
@@ -13,15 +14,52 @@ class SignUpDetail0127 extends StatefulWidget {
 class _SignUpDetail0127State extends State<SignUpDetail0127> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: const SignInAppBar(title: '세부프로필'),
-      body: Column(
-        children: [
-          const SignUpDetailsStepper(
-            currentStep: 2,
-            stage: '성향선택(3/4)',
-          ),
-        ],
+    return Scaffold(
+      appBar: ExitIconAppBar(),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 12.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Expanded(
+              flex: 1,
+              child: SizedBox(),
+            ),
+            Center(
+              child: Text(
+                textAlign: TextAlign.center,
+                '세부 프로필 입력이 완료되었어요!\n파티에 참여해볼까요?',
+                style: SignUp0115Styles.mainTitle,
+              ),
+            ),
+            const Expanded(
+              flex: 1,
+              child: SizedBox(),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: buildOutlinedShortButton(
+                    context,
+                    '프로필로 이동',
+                    '/',
+                  ),
+                ),
+                const SizedBox(
+                  width: 8.0,
+                ),
+                Expanded(
+                  child: buildFilledLongButton(
+                    context,
+                    '확인',
+                    '/main/1100',
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
