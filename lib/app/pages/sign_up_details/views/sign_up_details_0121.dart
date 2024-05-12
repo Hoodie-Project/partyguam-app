d3b89085fd2711146f18f506ae239d392ac0b346
ha-ccoon <codeminhasohn@gmail.com>
2024-05-12 22:08:52 +0900

    chore: add selected logic to the views

--- lib/app/pages/sign_up_details/views/sign_up_details_0121.dart
+++ lib/app/pages/sign_up_details/views/sign_up_details_0121.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:partyguam/app/widgets/snack_bar.dart';
 
 import '../../../theme/colors.dart';
 import '../../../theme/styles.dart';
+import '../../../utils/constants.dart';
 import '../../../widgets/app_bar.dart';
 import '../../../widgets/text.dart';
 import '../widgets/buttons.dart';
@@ -16,6 +18,34 @@ class SignUpDetail0121 extends StatefulWidget {
 }
 
 class _SignUpDetail0121State extends State<SignUpDetail0121> {
+  // province
+  final List<String> _provinceList =
+      Province.values.map((element) => element.option).toList();
+  final List _selectedProvinces = [];
+
+  void _selectProvinceTiles(int index) {
+    if (_selectedProvinces.isEmpty) {
+      _selectedProvinces.add(index);
+    } else {
+      _selectedProvinces.removeAt(0);
+      _selectedProvinces.add(index);
+    }
+  }
+
+  // city
+  final List<String> _cityList =
+      City.values.map((element) => element.option).toList();
+  final List _selectedCities = [];
+
+  void _selectCityTiles(int index) {
+    if (_selectedCities.length < 3) {
+      _selectedCities.add(index);
+    } else {
+      showWarningSnackBar(context, '최대 3개까지 선택할 수 있어요.');
+    }
+  }
+
+  //
   void _navigateToNextPage() {
     context.push('/sign_up/detail/0122');
   }
@@ -50,7 +80,7 @@ class _SignUpDetail0121State extends State<SignUpDetail0121> {
                     ),
                     Expanded(
                       flex: 2,
-                      child: _buildCityListTiles(),
+                      child: _buildCityGridView(),
                     ),
                   ],
                 ),
@@ -79,107 +109,129 @@ class _SignUpDetail0121State extends State<SignUpDetail0121> {
         height: 312.0,
         decoration: BoxDecoration(
           border: Border.all(color: AppColors.greyColors.shade200),
-          borderRadius: BorderRadius.circular(10.0),
+          borderRadius: BorderRadius.circular(16.0),
         ),
         child: ListView.builder(
           shrinkWrap: true,
           itemBuilder: (BuildContext context, int index) {
             return _buildListTiles(index);
           },
-          itemCount: 10,
+          itemCount: _provinceList.length,
         ),
       ),
     );
   }
 
   Widget _buildListTiles(int index) {
-    return Container(
-      height: 52.0,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16.0),
-        border: Border.all(color: Colors.transparent),
-      ),
-      child: Align(
-        alignment: Alignment.center,
-        child: Text(
-          '서울',
-          style: TextStyle(
-            color: AppColors.greyColors.shade400,
-            fontSize: 14,
-            fontWeight: FontWeight.w700,
+    final bool isSelected = _selectedProvinces.contains(index);
+
+    BorderRadius setBorderRadius() {
+      if (index == 0) {
+        return const BorderRadius.only(
+          topLeft: Radius.circular(16.0),
+          topRight: Radius.circular(16.0),
+        );
+      }
+
+      if (index == 5) {
+        return const BorderRadius.only(
+          bottomLeft: Radius.circular(16.0),
+          bottomRight: Radius.circular(16.0),
+        );
+      }
+
+      return BorderRadius.zero;
+    }
+
+    return InkWell(
+      splashFactory: NoSplash.splashFactory,
+      onTap: () {
+        setState(() {
+          _selectProvinceTiles(index);
+        });
+      },
+      child: Container(
+        height: 52.0,
+        decoration: BoxDecoration(
+          color: isSelected
+              ? AppColors.primaryLightColors.shade400
+              : AppColors.greyColors.shade50,
+          borderRadius: setBorderRadius(),
+          border: Border.all(color: Colors.transparent),
+        ),
+        child: Align(
+          alignment: Alignment.center,
+          child: Text(
+            _provinceList[index],
+            style: TextStyle(
+              color: isSelected
+                  ? AppColors.primaryDarkColors.shade200
+                  : AppColors.greyColors.shade400,
+              fontSize: 14,
+              fontWeight: FontWeight.w700,
+            ),
           ),
         ),
       ),
     );
   }
 
-  _buildCityListTiles() {
+  _buildCityGridView() {
     return SizedBox(
       child: Container(
         height: 312.0,
         decoration: BoxDecoration(
           border: Border.all(color: AppColors.greyColors.shade200),
-          borderRadius: BorderRadius.circular(10.0),
+          borderRadius: BorderRadius.circular(16.0),
         ),
         child: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2, // 2열로 설정
             childAspectRatio: 2.3, // 각 그리드 항목의 가로 세로 비율을 1.0으로 설정
           ),
-          itemCount: 20, // 그리드 아이템의 총 개수
+          itemCount: _cityList.length, // 그리드 아이템의 총 개수
           itemBuilder: (BuildContext context, int index) {
-            return GridTile(
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16.0),
-                  border: Border.all(color: Colors.transparent),
-                ),
-                child: Center(
-                  child: Text(
-                    '전체',
-                    style: TextStyle(
-                      color: AppColors.greyColors.shade600,
-                      fontSize: 14,
-                      fontWeight: FontWeight.normal,
-                    ),
-                  ),
-                ),
-              ),
-            );
+            return _buildGridTiles(index);
           },
         ),
       ),
     );
+  }
 
-    // return Container(
-    //   height: 52.0,
-    //   decoration: BoxDecoration(
-    //     color: Colors.transparent,
-    //     borderRadius: BorderRadius.circular(16.0),
-    //     border: Border.all(color: Colors.transparent),
-    //   ),
-    //   child: Row(
-    //     mainAxisAlignment: MainAxisAlignment.spaceAround,
-    //     children: [
-    //       Text(
-    //         '전체',
-    //         style: TextStyle(
-    //           color: AppColors.greyColors.shade600,
-    //           fontSize: 14.0,
-    //           fontWeight: FontWeight.normal,
-    //         ),
-    //       ),
-    //       Text(
-    //         '강남구',
-    //         style: TextStyle(
-    //           color: AppColors.greyColors.shade600,
-    //           fontSize: 14.0,
-    //           fontWeight: FontWeight.normal,
-    //         ),
-    //       ),
-    //     ],
-    //   ),
-    // );
+  Widget _buildGridTiles(int index) {
+    final bool isSelected = _selectedCities.contains(index);
+
+    return InkWell(
+      splashFactory: NoSplash.splashFactory,
+      onTap: () {
+        setState(() {
+          _selectCityTiles(index);
+        });
+      },
+      child: GridTile(
+        child: Container(
+          decoration: BoxDecoration(
+            color: AppColors.greyColors.shade50,
+            borderRadius: BorderRadius.circular(16.0),
+            border: isSelected
+                ? Border.all(color: AppColors.primaryLightColors)
+                : Border.all(color: Colors.transparent),
+          ),
+          child: Center(
+            child: Text(
+              _cityList[index],
+              style: TextStyle(
+                color: isSelected
+                    ? AppColors.primaryDarkColors.shade100
+                    : AppColors.greyColors.shade600,
+                fontSize: 14,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
   Widget _buildNextButton(BuildContext context) {
--- lib/app/utils/constants.dart
+++ lib/app/utils/constants.dart
@@ -20,7 +20,7 @@ enum Personality {
   final String option;
 }
 
-enum ConfidentList {
+enum Confident {
   option1('기술적인 역량과 전문 지식을 가지고 있어요.'),
   option2('효율적으로 일을 처리할 수 있어요.'),
   option3('시간관리 능력이 뛰어나요.'),
@@ -28,19 +28,57 @@ enum ConfidentList {
   option5('창의적인 아이디어가 많아요.'),
   option6('소통과 협업에 자신있어요.');
 
-  const ConfidentList(this.option);
+  const Confident(this.option);
 
   final String option;
 }
 
-enum ChallengeList {
+enum Challenge {
   option1('새로운 기술 및 업무에 도전하고 싶어요.'),
   option2('해당 분야의 경력을 쌓고 싶어요.'),
   option3('어려운 문제 및 상황을 풀어나가고 싶어요.'),
   option4('리더십 역량을 기르고 싶어요.'),
   option5('제한된 시간에서 능력을 확인하고 싶어요.');
 
-  const ChallengeList(this.option);
+  const Challenge(this.option);
+
+  final String option;
+}
+
+enum Province {
+  option1('서울'),
+  option2('경기'),
+  option3('인천'),
+  option4('강원'),
+  option5('충북'),
+  option6('충남'),
+  option7('전북'),
+  option8('전남');
+
+  const Province(this.option);
+
+  final String option;
+}
+
+enum City {
+  option1('전체'),
+  option2('강북구'),
+  option3('강남구'),
+  option4('강서구'),
+  option5('관악구'),
+  option6('광진구'),
+  option7('구로구'),
+  option8('노원구'),
+  option9('도봉구'),
+  option10('동작구'),
+  option11('동대문구'),
+  option12('사상구'),
+  option13('사하구'),
+  option14('중구'),
+  option15('동구'),
+  option16('서구');
+
+  const City(this.option);
 
   final String option;
 }
