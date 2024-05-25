0c7719d3060fdb16c37afc88fb553e5ee4bdf779
ha-ccoon <codeminhasohn@gmail.com>
2024-05-25 16:08:09 +0900

    chore: implement userCredential bloc
    - auth_bloc: where receive the user input, and emit the result (data, events etc)
    - auth_event: implement an event to the state management
    - auth_state: manage the states... loading states, errors etc

--- lib/presentation/pages/sign_up/bloc/auth_bloc.dart
+++ lib/presentation/pages/sign_up/bloc/auth_bloc.dart
@@ -1,13 +1,44 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
+import '../../../../domain/index.dart';
+
 part 'auth_event.dart';
 part 'auth_state.dart';
 
+/// https://youtu.be/_E3EF1jPumM?si=NOpVOasJ2RLaU4ux&t=20450
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc() : super(AuthInitial()) {
-    on<AuthEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  AuthBloc({
+    /// named constructors: to know what I have and where I give it to them
+    required SendUserCredentials sendUserCredentials,
+  })  : _sendUserCredentials = sendUserCredentials,
+
+        /// initiate _userCredential to userCredential
+        super(const AuthInitial()) {
+    on<SendUserCredentialsEvent>(_sendUserCredentialsHandler);
+  }
+
+  final SendUserCredentials _sendUserCredentials;
+
+  Future<void> _sendUserCredentialsHandler(
+    SendUserCredentialsEvent event,
+    Emitter<AuthState> emit,
+  ) async {
+    /// set loading state
+    emit(const SendingUserCredentials());
+
+    /// invoke the usecase
+    final result = await _sendUserCredentials(
+      SendUserCredentialParams(
+        uid: event.uid,
+        idToken: event.idToken,
+      ),
+    );
+
+    /// equatable syntax: result.fold(L,R)
+
+    result.fold(
+        (registered) => emit(AuthRegisteredToken(registered.signUpAccessToken)),
+        (r) => emit(const UserCredentialsSent()));
   }
 }
