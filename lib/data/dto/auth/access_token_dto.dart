1029c25b208dfbf147ff63439d7f380faa3a3ff2
ha-ccoon <codeminhasohn@gmail.com>
2024-06-09 16:51:12 +0900

    chore: create accessTokenDto

--- lib/data/dto/auth/access_token_dto.dart
+++ lib/data/dto/auth/access_token_dto.dart
@@ -1,24 +1,24 @@
 import 'package:json_annotation/json_annotation.dart';
 
-part 'auth_tokens_dto.g.dart';
+part 'access_token_dto.g.dart';
 
 @JsonSerializable()
-class AuthTokensDto {
-  AuthTokensDto({
-    required this.refreshToken,
+class AccessTokenDto {
+  AccessTokenDto({
+    required this.accessToken,
   });
 
   /// The generated code assumes these values exist in JSON.
   @JsonKey(required: true)
-  final String refreshToken;
+  final String accessToken;
 
   /// Connect the generated [_$PersonFromJson] function to the `fromJson`
   /// factory.
-  factory AuthTokensDto.fromJson(Map<String, dynamic> json) =>
-      _$AuthTokensDtoFromJson(json);
+  factory AccessTokenDto.fromJson(Map<String, dynamic> json) =>
+      _$AccessTokenDtoFromJson(json);
 
   /// Connect the generated [_$PersonToJson] function to the `toJson` method.
-  Map<String, dynamic> toJson() => _$AuthTokensDtoToJson(this);
+  Map<String, dynamic> toJson() => _$AccessTokenDtoToJson(this);
 }
 
 // dart run build_runner build --delete-conflicting-outputs
